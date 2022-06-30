597cb5f45502b0a68c83986b1d844406c5995748
wpcww <chront4n@gmail.com>
2022-06-30 16:17:39 +0800

    add material ui

--- reactwebsite/src/components/Create.js
+++ reactwebsite/src/components/Create.js
@@ -1,26 +1,52 @@
 import React, { useState } from "react";
 import "./Create.css";
-
-function generate(){
-    return(
-        <div></div>
-    )
-}
+import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
+import RemoveIcon from '@mui/icons-material/Remove';
 
 function Create() {
   const [input, setInput] = useState([
-    {Name:"", Requirement:[]}
+    {Name:"A", Requirement:[]},
+    {Name:"B", Requirement:[]}
   ])
+
+  const handleChangeInput = (id, event) => {
+    const newInputFields = input.map(i => {
+      if(id === i.id) {
+        i[event.target.name] = event.target.value
+      }
+      return i;
+    })
+    setInput(newInputFields);
+  }
+
+  const handleAddFields = () => {
+    setInput([...input, { Name:'',  Requirement:[] }])
+  }
+
+  const handleRemoveFields = id => {
+    const values  = [...input];
+    values.splice(values.findIndex(value => value.id === id), 1);
+    setInput(values);
+  }
+
   return(
     <>
         <div>Create template</div>
-        <form>
-          { input.map((records, i) => (
-            <div key={i}>
-              <input type="text"/>
-            </div>
-          ))}
-        </form>
+        <Container>
+          <form>
+            { input.map((records, i) => (
+              <div key={i}>
+                <TextField
+                  name="name"
+                  label="Project Name"
+                  variant="filled"
+                  value={input.Name}
+                />
+              </div>
+            ))}
+          </form> 
+        </Container>
     </>
 
 
