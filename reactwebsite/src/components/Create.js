dc169852adfc123e2c2e9a4806bcdec72e872f68
wpcww <chront4n@gmail.com>
2022-07-04 18:36:33 +0800

    Working on json path compression, runnable except single/option button

--- reactwebsite/src/components/Create.js
+++ reactwebsite/src/components/Create.js
@@ -3,10 +3,20 @@ import "./Create.css";
 import Container from '@mui/material/Container';
 import TextField from '@mui/material/TextField';
 import RemoveIcon from '@mui/icons-material/Remove';
+import { Button } from "@mui/material";
 
 function Create() {
   const [input, setInput] = useState(
-    {Name:"", Requirement:[{"Type":"Single","Value":["",""]}]},
+    {
+      "Name":"",
+      "Requirement":
+      [
+          {
+              "Type":"Single",
+              "Value":["",""]
+          }
+      ]
+   },
   )
 
   const handleChangeInput = (recKey, event) => {
@@ -15,9 +25,9 @@ function Create() {
     setInput(temp);
   }
 
-  const handleChangeRequirement = (iKey, recKey, event) => {
+  const handleChangeRequirement = (prev, iKey, recKey, event) => {
     const temp = JSON.parse(JSON.stringify(input));
-    temp["Requirement"][iKey]["Value"][recKey]=event.target.value
+    temp[prev][iKey]["Value"][recKey]=event.target.value
     setInput(temp);
   }
 
@@ -33,28 +43,85 @@ function Create() {
     setInput(temp)
   }
 
-  const reqTemplate = input["Requirement"].map((item, iKey) => {
-    return(
-      <>
-      <div key={iKey.toString()}>
-        <TextField
-          name="Requirement"
-          label="Requirement Item"
-          variant="filled"
-          value={item["Value"][0]}
-          onChange={event => handleChangeRequirement(iKey, 0, event)}
-        />
-        <TextField
-          name="Requirement"
-          label="URL(Optional)"
-          variant="filled"
-          value={item["Value"][1]}
-          onChange={event => handleChangeRequirement(iKey, 1, event)}
-        />
-        <RemoveIcon type="button" onClick={() => handleRemoveField(iKey)} />
+  const toggleType = (prev, iKey, typeString) => {
+    const temp = JSON.parse(JSON.stringify(input));
+    temp.prev.iKey.Type=typeString;
+    temp.prev.iKey.Branch=[{"":""}];
+    setInput(temp) 
+  }
+
+  const pathMaker = (prev) => {
+    var layer = {}
+    layer.each(function(){
+      layer[this.id] = (this).val();
+    })
+    return JSON.stringify(layer)
+  }
+
+  const typeBranching = (prev,json,iKey) =>{
+    if (json["Type"] === "Single"){
+      return(
+        <>
+        <div>Requirement {iKey}</div>
+        <div>
+          <Button onClick={() => toggleType(prev,iKey,"Single")}>Single
+          </Button>
+          <Button onClick={() => toggleType(prev,iKey,"Option")}>Option
+          </Button>
         </div>
-    </>
-    )
+        <div key={iKey.toString()}>
+          <TextField
+            name="Requirement"
+            label="Requirement Item"
+            variant="filled"
+            value={json["Value"][0]}
+            onChange={event => handleChangeRequirement(prev, iKey, 0, event)}
+          />
+          <TextField
+            name="Requirement"
+            label="URL(Optional)"
+            variant="filled"
+            value={json["Value"][1]}
+            onChange={event => handleChangeRequirement(prev, iKey, 1, event)}
+          />
+          <RemoveIcon type="button" onClick={() => handleRemoveField(iKey)} />
+          </div>
+      </>
+      )
+
+    }else{
+      return(
+        <>
+          <div>
+            Available choices:
+            <TextField
+            name="Requirement"
+            label="Branch Name"
+            variant="filled"
+            value={json["Value"][0]}
+            //onChange={event => handleChangeRequirement(iKey, 1, event)}
+            />
+            {
+              json.Branch.map((item,index) => {
+                prev = prev.Branch
+                return(typeBranching(prev,item,index))
+              })
+            }
+          </div>
+          <div>
+
+          </div>
+        </>
+      )
+    }
+  }
+
+  const reqTemplate = input["Requirement"].map((item, iKey) => {
+      return(
+        <>
+        {typeBranching("Requirement",item,iKey)}
+      </>
+      )    
 
   }
   )
@@ -84,12 +151,7 @@ function Create() {
           </form> 
         </Container>
     </>
-
-
   )
-
-  
-  
 }
 
 export default Create;
