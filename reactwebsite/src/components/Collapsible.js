867b52918d55cc83f7022e31d98d9e0de5d30942
wpcww <60952195+wpcww@users.noreply.github.com>
2022-08-08 11:10:53 +0800

    Updated API URL to the latest, Runnable

--- reactwebsite/src/components/Collapsible.js
+++ reactwebsite/src/components/Collapsible.js
@@ -1,152 +1,159 @@
-import React, {useState, useEffect} from 'react'
-import useCollapse from 'react-collapsed';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Checkbox from '@mui/material/Checkbox';
-import './Collapsible.css';
+import React, { useState, useEffect } from "react";
+import useCollapse from "react-collapsed";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Checkbox from "@mui/material/Checkbox";
+import "./Collapsible.css";
 function Section(props) {
-    const config = {
-        defaultExpanded: props.defaultExpanded || false,
-        collapsedHeight: props.collapsedHeight || 0
-    };
-    const { getCollapseProps, getToggleProps, isExpanded } = useCollapse(config);
-return (
+  const config = {
+    defaultExpanded: props.defaultExpanded || false,
+    collapsedHeight: props.collapsedHeight || 0,
+  };
+  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse(config);
+  return (
     <div className="collapsible">
-        <div className="header" {...getToggleProps()}>
-            <div className="title">{props.title}</div>
-            <div className="icon">
-                <i className={'fas fa-chevron-circle-' + (isExpanded ? 'up' : 'down')}></i>
-            </div>
+      <div className="header" {...getToggleProps()}>
+        <div className="title">{props.title}</div>
+        <div className="icon">
+          <i
+            className={"fas fa-chevron-circle-" + (isExpanded ? "up" : "down")}
+          ></i>
         </div>
-        <div {...getCollapseProps()}>
-            <div className="content">
-                {props.children}
-            </div>
-        </div>
+      </div>
+      <div {...getCollapseProps()}>
+        <div className="content">{props.children}</div>
+      </div>
     </div>
-    );
+  );
 }
 
 function Collapsible() {
-    
-    const [record, getData] = useState([])
-    const URL = 'https://eszevlom66.execute-api.ap-east-1.amazonaws.com/default/joblist'
-    //const CataURL = 'https://s3.amazonaws.com/pocbucket2.brian/test3.json'
+  const [record, getData] = useState([]);
+  const URL = "https://zwcpq1a6qg.execute-api.ap-east-1.amazonaws.com/dev/list";
+  //const CataURL = 'https://s3.amazonaws.com/pocbucket2.brian/test3.json'
 
-    useEffect(() => {
-        fetchData()
-    }, [])
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const fetchData = () => {
-        
-        fetch(URL)
-          .then((res) =>
-            res.json())
-     
-          .then((response) => {
-            //console.log(response);
-            getData(response);
-          })
-    }
+  const fetchData = () => {
+    fetch(URL)
+      .then((res) => res.json())
 
-    const DisplayMultiple = (props) => {
-        const index = props.objKey
-        const data = props.content
-        return (
-            <>
-                {/* <div><input type="checkbox"/>{data[index].Value[0]}:</div> */}
-                <ListItem>
-                  <ListItemIcon>
-                    <Checkbox />
-                  </ListItemIcon>
-                  <ListItemText primary={data[index].Value[0]} />
-                </ListItem>
-                
-                <div className='newBranch'>
-                {
-                    data[index].Successor.map(item =>{
-                        if (data[item].Predecessor === index && data[item].Successor.length === 0){
-                          return(<DisplaySingle objKey={item} content={data} key={item + "DS"}/>)
-                        }else if(data[item].Successor.length !== 0 && item !== "0"){
-                          return(<DisplayMultiple objKey={item} content={data} key={item + "DM"}/>)
-                        }return null
-                    }
-                  )
-                }
-                
-                </div>
+      .then((response) => {
+        //console.log(response);
+        getData(response);
+      });
+  };
 
-            </>
-        )
-    }
-
-    const DisplaySingle = (props) => {
-        const index = props.objKey
-        const data = props.content
-        //console.log(data[index])
-        return (
-            <>
-                <div className='reqDiv'>
-                        {/* <div className='reqDivItem'><input type="checkbox"/>{data[index].Value[0]}</div> */}
-                        {/* <div className='reqDivItem'><a href={data[index].Value[1]}>{data[index].Value[1]}</a></div> */}
-                        <ListItem>
-                          <ListItemIcon>
-                            <Checkbox />
-                          </ListItemIcon>
-                          <ListItemText primary={data[index].Value[0]} secondary={data[index].Value[1]}/>
-                        </ListItem>
-                </div>
-            </>
-        )
-    }
-
-    const RequirementDisplay = (props) => {
-        //console.log()
-        var reqJson = props.reqJson
-        var displayList = []
-        Object.keys(reqJson).map(item => {
-        //console.log(reqJson[item])
-          if(reqJson[item].Predecessor === "0" && reqJson[item].Successor.length === 0){
-            //console.log("Single: " + reqJson[item].Value[0])
-            displayList.push(
-              <div key={item + "DS"}>
-                <DisplaySingle objKey={item} content={reqJson}/>
-              </div>
-            )
-          }else if(reqJson[item].Successor.length !== 0 && item !== "0" && reqJson[item].Predecessor === "0"){
-            //console.log("Multi: " + reqJson[item].Value[0])
-            displayList.push(
-              <div key={item + "DM"}>
-                <DisplayMultiple objKey={item} content={reqJson}/>
-              </div>
-            )
-          }return null
-        }
-        )
-        return displayList
-      }
-
-    const itemList2 = record.map((reqItem, i) => {
-        return (
-            <Section key={i} title={reqItem.Name}>
-              <List>
-                <div>Requirements:</div>
-                <RequirementDisplay reqJson = {reqItem.Requirement}/>
-              </List>
-            </Section>
-        )
-
-    })
-
+  const DisplayMultiple = (props) => {
+    const index = props.objKey;
+    const data = props.content;
     return (
-        <div className="preferences">
+      <>
+        {/* <div><input type="checkbox"/>{data[index].Value[0]}:</div> */}
+        <ListItem>
+          <ListItemIcon>
+            <Checkbox />
+          </ListItemIcon>
+          <ListItemText primary={data[index].Value[0]} />
+        </ListItem>
 
-            {itemList2}
-
-       </div>
+        <div className="newBranch">
+          {data[index].Successor.map((item) => {
+            if (
+              data[item].Predecessor === index &&
+              data[item].Successor.length === 0
+            ) {
+              return (
+                <DisplaySingle objKey={item} content={data} key={item + "DS"} />
+              );
+            } else if (data[item].Successor.length !== 0 && item !== "0") {
+              return (
+                <DisplayMultiple
+                  objKey={item}
+                  content={data}
+                  key={item + "DM"}
+                />
+              );
+            }
+            return null;
+          })}
+        </div>
+      </>
     );
+  };
+
+  const DisplaySingle = (props) => {
+    const index = props.objKey;
+    const data = props.content;
+    //console.log(data[index])
+    return (
+      <>
+        <div className="reqDiv">
+          {/* <div className='reqDivItem'><input type="checkbox"/>{data[index].Value[0]}</div> */}
+          {/* <div className='reqDivItem'><a href={data[index].Value[1]}>{data[index].Value[1]}</a></div> */}
+          <ListItem>
+            <ListItemIcon>
+              <Checkbox />
+            </ListItemIcon>
+            <ListItemText
+              primary={data[index].Value[0]}
+              secondary={data[index].Value[1]}
+            />
+          </ListItem>
+        </div>
+      </>
+    );
+  };
+
+  const RequirementDisplay = (props) => {
+    //console.log()
+    var reqJson = props.reqJson;
+    var displayList = [];
+    Object.keys(reqJson).map((item) => {
+      //console.log(reqJson[item])
+      if (
+        reqJson[item].Predecessor === "0" &&
+        reqJson[item].Successor.length === 0
+      ) {
+        //console.log("Single: " + reqJson[item].Value[0])
+        displayList.push(
+          <div key={item + "DS"}>
+            <DisplaySingle objKey={item} content={reqJson} />
+          </div>
+        );
+      } else if (
+        reqJson[item].Successor.length !== 0 &&
+        item !== "0" &&
+        reqJson[item].Predecessor === "0"
+      ) {
+        //console.log("Multi: " + reqJson[item].Value[0])
+        displayList.push(
+          <div key={item + "DM"}>
+            <DisplayMultiple objKey={item} content={reqJson} />
+          </div>
+        );
+      }
+      return null;
+    });
+    return displayList;
+  };
+
+  const itemList2 = record.map((reqItem, i) => {
+    return (
+      <Section key={i} title={reqItem.Name}>
+        <List>
+          <div>Requirements:</div>
+          <RequirementDisplay reqJson={reqItem.Requirement} />
+        </List>
+      </Section>
+    );
+  });
+
+  return <div className="preferences">{itemList2}</div>;
 }
 
 export default Collapsible;
--- reactwebsite/src/components/manage.js
+++ reactwebsite/src/components/manage.js
@@ -1,204 +1,240 @@
-import { Button, Dialog , DialogTitle, DialogContent } from '@mui/material';
-import { DialogContentText , DialogActions } from '@mui/material';
-import React, {useState, useEffect, useRef } from 'react'
-import useCollapse from 'react-collapsed';
-import './Manage.css';
-import Create from './Create';
+import { Button, Dialog, DialogTitle, DialogContent } from "@mui/material";
+import { DialogContentText, DialogActions } from "@mui/material";
+import React, { useState, useEffect, useRef } from "react";
+import useCollapse from "react-collapsed";
+import "./Manage.css";
+import Create from "./Create";
 
-function Manage({state, setState, structClone, data}) {
-    const [record, getData] = useState([])
-    const URL = 'https://eszevlom66.execute-api.ap-east-1.amazonaws.com/default/joblist'
-    //const CataURL = 'https://s3.amazonaws.com/pocbucket2.brian/test3.json'
+function Manage({ state, setState, structClone, data }) {
+  const [record, getData] = useState([]);
+  const URL = "https://zwcpq1a6qg.execute-api.ap-east-1.amazonaws.com/dev/list";
+  //const CataURL = 'https://s3.amazonaws.com/pocbucket2.brian/test3.json'
 
-    useEffect(() => {
-        fetchData()
-    }, [])
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const fetchData = () => {
-        fetch(URL)
-          .then((res) =>
-            res.json())
-     
-          .then((response) => {
-            //console.log(response);
-            const temp = JSON.parse(JSON.stringify(response))
-            getData(temp);
-          }) 
-    }
+  const fetchData = () => {
+    fetch(URL)
+      .then((res) => res.json())
 
-    const deletePending = useRef("")
+      .then((response) => {
+        //console.log(response);
+        const temp = JSON.parse(JSON.stringify(response));
+        getData(temp);
+      });
+  };
 
-    const [open, setOpen] = useState(false);
+  const deletePending = useRef("");
 
-    const handleOpen = () => {
-        setOpen(true);
-    };
-    
-    const handleClose = () => {
-        setOpen(false);
-    };
+  const [open, setOpen] = useState(false);
 
-    const Alert = () => {
-        return(
-            <div>
-                <Dialog 
-                    open={open}
-                    onClose={handleClose} 
-                >
-                    <DialogTitle id="deleteConfirm">
-                    {"Are you sure?"}
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                    The record will be removed.
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={() => {
-                        remove(deletePending.current)
-                        handleClose()
-                    }} color="primary">
-                    Confirm
-                    </Button>
-                    <Button onClick={() => handleClose()} color="primary" autoFocus>
-                    Cancel
-                    </Button>
-                </DialogActions>      
-                </Dialog>   
-            </div>
-        )
-    }
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
-    function Section(props) {
-        const config = {
-            defaultExpanded: props.defaultExpanded || false,
-            collapsedHeight: props.collapsedHeight || 0
-        };
-        const { getCollapseProps, getToggleProps, isExpanded } = useCollapse(config);
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const Alert = () => {
     return (
-        <div className="collapsible">
-            <div className="header" {...getToggleProps()}>
-                <div className="title">{props.title.Name}
-                
-                <Button onClick={() => {
-                    structClone.current = props.title.Requirement
-                    Object.keys(props.title.Requirement).forEach((item) => {
-                        if(item !== "0"){
-                            data.current[item]=[props.title.Requirement[item].Value[0],props.title.Requirement[item].Value[1]]
-                        }
-                    })
-                    const temp = JSON.parse(JSON.stringify(structClone.current))
-                    setState({structField: temp, nameField: props.title.Name})
+      <div>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle id="deleteConfirm">{"Are you sure?"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              The record will be removed.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              onClick={() => {
+                remove(deletePending.current);
+                handleClose();
+              }}
+              color="primary"
+            >
+              Confirm
+            </Button>
+            <Button onClick={() => handleClose()} color="primary" autoFocus>
+              Cancel
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  };
 
-                }}>Edit</Button>
-                <Button onClick={() => 
-                    {
-                        deletePending.current = props.title.Name
-                        handleOpen()
-                        
-                    }}>Remove</Button>
-                </div>
-                <div className="icon">
-                    <i className={'fas fa-chevron-circle-' + (isExpanded ? 'up' : 'down')}></i>
-                </div>
-            </div>
-            <div {...getCollapseProps()}>
-                <div className="content">
-                    {props.children}
-                </div>
-            </div>
+  function Section(props) {
+    const config = {
+      defaultExpanded: props.defaultExpanded || false,
+      collapsedHeight: props.collapsedHeight || 0,
+    };
+    const { getCollapseProps, getToggleProps, isExpanded } =
+      useCollapse(config);
+    return (
+      <div className="collapsible">
+        <div className="header" {...getToggleProps()}>
+          <div className="title">
+            {props.title.Name}
+
+            <Button
+              onClick={() => {
+                structClone.current = props.title.Requirement;
+                Object.keys(props.title.Requirement).forEach((item) => {
+                  if (item !== "0") {
+                    data.current[item] = [
+                      props.title.Requirement[item].Value[0],
+                      props.title.Requirement[item].Value[1],
+                    ];
+                  }
+                });
+                const temp = JSON.parse(JSON.stringify(structClone.current));
+                setState({ structField: temp, nameField: props.title.Name });
+              }}
+            >
+              Edit
+            </Button>
+            <Button
+              onClick={() => {
+                deletePending.current = props.title.Name;
+                handleOpen();
+              }}
+            >
+              Remove
+            </Button>
+          </div>
+          <div className="icon">
+            <i
+              className={
+                "fas fa-chevron-circle-" + (isExpanded ? "up" : "down")
+              }
+            ></i>
+          </div>
         </div>
-        );
-    }
+        <div {...getCollapseProps()}>
+          <div className="content">{props.children}</div>
+        </div>
+      </div>
+    );
+  }
 
-    const remove = async (Name) => {
-        await fetch('https://5msl1adfyb.execute-api.ap-east-1.amazonaws.com/test/remove', {
-        method: 'DELETE',
-        body: JSON.stringify({"Name":Name})
-        })
-        window.location.reload()
-    }
-
-    const DisplayMultiple = (props) => {
-        const index = props.objKey
-        const data = props.content
-        return (
-            <>
-                <div><input type="checkbox"/>{data[index].Value[0]}:</div>
-                
-                <div className='newBranch'>
-                {
-                    data[index].Successor.map(item =>{
-                        if (data[item].Predecessor === index && data[item].Successor.length === 0){
-                          return(<DisplaySingle objKey={item} content={data} key={item + "DS"}/>)
-                        }else if(data[item].Successor.length !== 0 && item !== "0"){
-                          return(<DisplayMultiple objKey={item} content={data} key={item + "DM"}/>)
-                        }return null
-                    })
-                }
-                
-                </div>
-            </>
-        )
-    }
-
-    const DisplaySingle = (props) => {
-        const index = props.objKey
-        const data = props.content
-        return (
-            <>
-                <div className='reqDiv'>
-                        <div className='reqDivItem'><input type="checkbox"/>{data[index].Value[0]}</div>
-                        <div className='reqDivItem'><a href={data[index].Value[1]}>{data[index].Value[1]}</a></div>
-                </div>
-            </>
-        )
-    }
-
-    const RequirementDisplay = (props) => {
-        var reqJson = props.reqJson
-        var displayList = []
-        Object.keys(reqJson).forEach(item => {
-          if(reqJson[item].Predecessor === "0" && reqJson[item].Successor.length === 0){
-            displayList.push(
-              <div key={item + "DS"}>
-                <DisplaySingle objKey={item} content={reqJson}/>
-              </div>
-            )
-          }else if(reqJson[item].Successor.length !== 0 && item !== "0" && reqJson[item].Predecessor === "0"){
-            displayList.push(
-              <div key={item + "DM"}>
-                <DisplayMultiple objKey={item} content={reqJson}/>
-              </div>
-            )
-          }
-        }
-        )
-        return displayList
+  const remove = async (Name) => {
+    await fetch(
+      "https://zwcpq1a6qg.execute-api.ap-east-1.amazonaws.com/dev/delete",
+      {
+        method: "DELETE",
+        body: JSON.stringify({ Name: Name }),
       }
+    );
+    window.location.reload();
+  };
 
-    const itemList = record.map((reqItem, i) => {
-        return (
-            <Section key={i} title={reqItem}>
-                <div>Requirements:</div>
-                <RequirementDisplay reqJson = {reqItem.Requirement} key={reqItem}/>
-                
-            </Section>
-        )
-
-    })
-
+  const DisplayMultiple = (props) => {
+    const index = props.objKey;
+    const data = props.content;
     return (
-        <div className='flexContainer'>
-            <div className="preferences">
-                {itemList}
-            </div>
-            <div className="editArea">
-                <Create state={state} setState={setState} structClone={structClone} data={data}/>
-            </div>
-            <Alert />
+      <>
+        <div>
+          <input type="checkbox" />
+          {data[index].Value[0]}:
         </div>
+
+        <div className="newBranch">
+          {data[index].Successor.map((item) => {
+            if (
+              data[item].Predecessor === index &&
+              data[item].Successor.length === 0
+            ) {
+              return (
+                <DisplaySingle objKey={item} content={data} key={item + "DS"} />
+              );
+            } else if (data[item].Successor.length !== 0 && item !== "0") {
+              return (
+                <DisplayMultiple
+                  objKey={item}
+                  content={data}
+                  key={item + "DM"}
+                />
+              );
+            }
+            return null;
+          })}
+        </div>
+      </>
+    );
+  };
+
+  const DisplaySingle = (props) => {
+    const index = props.objKey;
+    const data = props.content;
+    return (
+      <>
+        <div className="reqDiv">
+          <div className="reqDivItem">
+            <input type="checkbox" />
+            {data[index].Value[0]}
+          </div>
+          <div className="reqDivItem">
+            <a href={data[index].Value[1]}>{data[index].Value[1]}</a>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  const RequirementDisplay = (props) => {
+    var reqJson = props.reqJson;
+    var displayList = [];
+    Object.keys(reqJson).forEach((item) => {
+      if (
+        reqJson[item].Predecessor === "0" &&
+        reqJson[item].Successor.length === 0
+      ) {
+        displayList.push(
+          <div key={item + "DS"}>
+            <DisplaySingle objKey={item} content={reqJson} />
+          </div>
         );
+      } else if (
+        reqJson[item].Successor.length !== 0 &&
+        item !== "0" &&
+        reqJson[item].Predecessor === "0"
+      ) {
+        displayList.push(
+          <div key={item + "DM"}>
+            <DisplayMultiple objKey={item} content={reqJson} />
+          </div>
+        );
+      }
+    });
+    return displayList;
+  };
+
+  const itemList = record.map((reqItem, i) => {
+    return (
+      <Section key={i} title={reqItem}>
+        <div>Requirements:</div>
+        <RequirementDisplay reqJson={reqItem.Requirement} key={reqItem} />
+      </Section>
+    );
+  });
+
+  return (
+    <div className="flexContainer">
+      <div className="preferences">{itemList}</div>
+      <div className="editArea">
+        <Create
+          state={state}
+          setState={setState}
+          structClone={structClone}
+          data={data}
+        />
+      </div>
+      <Alert />
+    </div>
+  );
 }
 
 export default Manage;
